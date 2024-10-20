0389969c3aca400a786f347c53f114635b7c8b19
Tayyab Hussain <tayyab.hussain.it@gmail.com>
2024-10-20 22:38:37 +0400

    details in readme.me - inprogress

--- app/Enums/MenuItem.php
+++ app/Enums/MenuItem.php
@@ -1,7 +1,29 @@
 <?php
 
+/**
+ * MenuItem enum file
+ *
+ * PHP Version 8.3
+ *
+ * @category Enum
+ * @package  Console_Command
+ * @author   Tayyab <tayyab.hussain.it@gmail.com>
+ * @license  https://github.com/tayyabhussainit Private Repo
+ * @link     https://github.com/tayyabhussainit/flashcard
+ */
 namespace App\Enums;
 
+/**
+ * MenuItem enum
+ *
+ * The enum is to keep menu items
+ *
+ * @category ENUM
+ * @package  Console_Command
+ * @author   Tayyab <tayyab.hussain.it@gmail.com>
+ * @license  https://github.com/tayyabhussainit Private Repo
+ * @link     https://github.com/tayyabhussainit/flashcard
+ */
 enum MenuItem: string
 {
     case CREATE_FLASHCARD = 'Create Flashcard';
